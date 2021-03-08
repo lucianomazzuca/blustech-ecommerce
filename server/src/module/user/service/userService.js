a2be1b137635a59a3c3ba501363c0b7c167e3060
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-03-08 17:54:17 -0300

    Add save method to UserRepository

--- server/src/module/user/service/userService.js
+++ server/src/module/user/service/userService.js
@@ -1,10 +1,18 @@
-
+const User = require('../entity/User');
+const UserNotDefinedError = require('../error/UserNotDefinedError');
 
 class UserService {
-  constructor(userRepository) {
-
+  constructor({ userRepository }) {
+    this.userRepository = userRepository;
   }
 
+  async save(user) {
+    if (! (user instanceof User)) {
+      throw new UserNotDefinedError();
+    }
+
+    return this.userRepository.save(user);
+  };
 
 }
 
