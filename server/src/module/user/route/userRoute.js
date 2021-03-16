f9117ae339ca1ae8d609e10acf9c948d62dbdd66
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-03-16 20:28:56 -0300

    Add user context and login now saves jwt in local storage

--- server/src/module/user/route/userRoute.js
+++ server/src/module/user/route/userRoute.js
@@ -7,6 +7,6 @@ const router = express.Router();
 const userController = container.resolve('userController');
 router.get('/', passport.authenticate('jwt', { session: false }), userController.index.bind(userController));
 router.post('/login', userController.login.bind(userController));
-router.get('/details', passport.authenticate('jwt', { session: false}), userController.getUser.bind(userController));
+router.get('/me', passport.authenticate('jwt', { session: false}), userController.getUser.bind(userController));
 
 module.exports = router;
