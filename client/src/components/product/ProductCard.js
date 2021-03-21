274c10af80ae5134a3cf532f4152f8a1c2cf8741
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-03-21 20:48:28 -0300

    Add img src to product card

--- client/src/components/product/ProductCard.js
+++ client/src/components/product/ProductCard.js
@@ -1,8 +1,18 @@
-const ProductCard = ({ model, price, brand }) => {
+import imgNotAvailable from '../../img/not_available.png';
+
+const ProductCard = ({ model, price, brand, image }) => {
+  let imgSrc;
+  
+  if (process.env.REACT_APP_API_URL) {
+    imgSrc = `${process.env.REACT_APP_API_URL}/img/uploads/${image}`;
+  } else {
+    imgSrc = `http://localhost:5000/img/uploads/${image}`
+  }
+  
   return (
     <div className="product-card flex flex-col border-2 border-gray-200 rounded-lg overflow-hidden">
       <img
-        src="http://localhost:5000/img/uploads/3080.jpg"
+        src={image ? imgSrc : imgNotAvailable}
         alt="Product"
         className="w-full h-48 object-cover object-center"
       ></img>
