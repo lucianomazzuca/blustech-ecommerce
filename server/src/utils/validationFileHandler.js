61a38b25cc96a2d467031395d0a24460ca19a37d
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-03-30 16:17:22 -0300

    Add: on product validation, if an image is uploaded but the body validation fails, then the image will be deleted

--- server/src/utils/validationFileHandler.js
+++ server/src/utils/validationFileHandler.js
@@ -1,6 +1,14 @@
 const { validationResult } = require('express-validator');
 const fs = require('fs');
 
+function deleteFile(path) {
+  try {
+    fs.unlinkSync(path);
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 const validationHandler = (req, res, next) => {
   const error = validationResult(req);
 
@@ -9,7 +17,8 @@ const validationHandler = (req, res, next) => {
   };
 
   if (req.file) {
-    console.log(req.file)
+    console.log('validationFileHandle', req.file.path)
+    deleteFile(req.file.path)
   }
 
   return res.status(400).json({
