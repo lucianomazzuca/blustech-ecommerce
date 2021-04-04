4fa295b79df991cb0e6799fd0688c4c8d09f0513
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-04-03 22:51:10 -0300

    Add styles to product detail page

--- client/src/pages/ProductDetail.js
+++ client/src/pages/ProductDetail.js
@@ -6,20 +6,29 @@ const ProductDetail = () => {
   let { productId } = useParams();
   const { data: product, error } = useSWR(`/products/${productId}`);
 
-
   if (error) return <div>Error</div>;
   if (!product) return <div>loading...</div>;
 
   return (
-    <div className="container-general">
-      <div className="bg-white mt-8 shadow-md p-4 border border-gray-200">
-        <h4 className="text-2xl pb-3 border-b border-gray-200">
-          {product.category.name} {product.brand.name} {product.model}
-        </h4>
+    <div className="container mx-auto max-w-screen-lg px-4">
+      <div className="grid grid-cols-1 gap-4 bg-white mt-8 shadow-md p-4 border border-gray-200 md:grid-cols-2">
+        <div className="">
+          <ProductImage filename={product.image} />
+        </div>
 
-          <div className="">
-            <ProductImage filename={product.image} />
+        <div className="">
+          <h4 className="text-2xl pb-3 border-b border-gray-200">
+            {product.category.name} {product.brand.name} {product.model}
+          </h4>
+          <div className="mt-4">{product.description}</div>
+
+          <div className="flex flex-col self-end justify-center items-center mt-4">
+            <span className="text-2xl font-semibold text-blue-700">${product.price}</span>
+            <button className="btn-primary mt-2">Add to cart</button>
           </div>
+
+        </div>
+        
       </div>
     </div>
   );
