d9a62bbedfa84c9f4ba243131d3bef759573891f
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-05-03 20:10:34 -0300

    Update database name in migration config file

--- server/data/config.js
+++ server/data/config.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     dialect: 'postgres',
     host: process.env.DB_HOST,
-    database: 'test',
+    database: 'blustech',
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD
   },
